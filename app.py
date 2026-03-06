2ecb7659beb1e6f955cba1fd75b31ce05b3c6c17
DiegoHG-dev-M <l20590254@sjuanrio.tecnm.mx>
2026-03-06 14:54:38 -0600

    Revert "Feat: Index"

--- app.py
+++ app.py
@@ -1,53 +1,11 @@
 import os
-from flask import Flask, request, jsonify, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
+from flask import Flask
 
-#cargar variables de entorno
-load_dotenv()
-#crear instancia
-app =  Flask(__name__)
+app = Flask(__name__)
 
-# Configuración de la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-#Modelo de la base de datos
-class Estudiante(db.Model):
-    __tablename__ = 'estudiantes'
-    no_control = db.Column(db.String, primary_key=True)
-    nombre = db.Column(db.String)
-    ap_paterno = db.Column(db.String)
-    ap_materno = db.Column(db.String)
-    semestre = db.Column(db.Integer)
-
-    def to_dict(self):
-        return{
-            'no_control': self.no_control,
-            'nombre': self.nombre,
-            'ap_paterno': self.ap_paterno,
-            'ap_materno': self.ap_materno,
-            'semestre': self.semestre,
-        }
-#Crear tablas si no existen
-with app.app_context():
-    db.create_all()
-
-#@app.route("/")
-#def home():
-#    return "Hola Mundo"
-
-#Ruta raiz
-@app.route('/')
-def index():
-    #retornar los alumnos
-    #return 'Hola Mundo'
-
-    #Trae todos los estudiantes
-    estudiantes = Estudiante.query.all()
-    return render_template('index.html', estudiantes = estudiantes)
+@app.route("/")
+def home():
+    return "Hola Mundo"
 
 if __name__ == '__main__':
     app.run(debug=True)
